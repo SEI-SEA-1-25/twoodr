c1476af300ace60324943cdd45e1ee8558378c4a
wonkatin <j.drawers@gmail.com>
2021-01-28 17:17:51 -0600

    Initial Commit

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-document.querySelector('#submit-twood').addEventListener('click', () => {
-  const newText = document.querySelector('#twood-input').value
-  // document.querySelector('.twood').innerText = newText
-  document.querySelector('#twood-input').value = ''
+let textField = document.getElementById("twood-input").value;
+console.log(textField);
+function buttonClick() {
+  const button = document.getElementById("submit-twood");
+  console.log("hello world");
+};
 
-  const newTwood = document.createElement('div')
-  document.querySelector('.twoods-container').append(newTwood)
-  // newTwood.classList.add('twood')
-  // newTwood.innerText = newText
+buttonClick()
 
+//document.addEventListener("click", buttonClick());
 
-})
-
-setTimeout(() => {
-  document.querySelector('.cutoff').classList.remove('hidden')  
-}, 200000)
+// function myFunction() {
+//   var x = document.getElementById("myBtn");
+//   x.disabled = true;
