85a804622c09c1b1526fe9c245bdad78ba141a89
wonkatin <j.drawers@gmail.com>
2021-01-28 21:12:22 -0600

    Finished Lab

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-
-
-//console.log(textField);
-
-const button = document.getElementById("submit-twood");
-//console.log(button)
-
-
+const button = document.getElementById("submit-twood")
 button.addEventListener('click', function(){
   let textField = document.getElementById("twood-input").value;
-  let userInput = document.querySelector(".twood");
-  userInput.innerText = textField
-  console.log(userInput)
-})
+  //let userInput = document.querySelector(".twood");
+  //userInput.innerText = textField;
+  newText = document.createElement("div");
+  let parent = document.querySelector(".twoods-container");
+  newText.className = "twood"
+  newText.innerText = textField;
+  parent.append(newText);
+});
   
-var message = setTimeout(function(){
-  var test = document.querySelector(".cutoff");
-  test.classList.remove(".hidden");
-  console.log(test)
-}, 800);
+// var message = setTimeout(function(){
+//   var test = document.querySelector(".cutoff");
+//   test.classList.remove("hidden");
+// }, 1000)
 
-
-
-
-
-
-//document.addEventListener("click", buttonClick());
-
-// function myFunction() {
-//   var x = document.getElementById("myBtn");
-//   x.disabled = true;
