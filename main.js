aa9f2c45f70a8bf9b55e553725510e343efee1b9
henry-mcdonald <a.henrymcdonald.a@gmail.com>
2021-01-28 19:44:32 -0600

    complete lab

--- main.js
+++ main.js
@@ -1,28 +1,90 @@
+////////////////////////////////////////////////////////
+////// Section 1/////////////////////////////////////
+///////////////////////////////////////////////////
+
+// const twoodButton = document.querySelector('#submit-twood');
+// twoodButton.addEventListener('click',sendTwood);
+
+// function sendTwood(){
+//   // getText()
+//   const txtInputButton = document.querySelector('#twood-input');
+//   const txt = txtInputButton.value;
+//   console.log(txt);
+//   if(txt === ''){
+//   alert('Please enter some text');
+//   } else{
+
+//     // overwriteExistingText
+
+//     displayText = document.querySelector('.twood');
+//     displayText.innerText = txt;
+//   }
+
+// }
+
+
+////////////////////////////////////////////////////////
+////// Section 2/////////////////////////////////////
+///////////////////////////////////////////////////
+
+// identify the web element we want to modify and assign it to a javascript variable
+// add a timeout (like a listener)
+// write a function to modify the web element in the way we want
+
+// chain the function to the listener
+
+//webElement.removeAttribute("display");
+
+
+const webElement = document.querySelector(".cutoff");
+
+function makeWebElementVisible(){
+  webElement.classList.remove("hidden");
+}
+
+setTimeout(function(){ makeWebElementVisible(); }, 20000);
+
+
+////////////////////////////////////////////////////////
+////// Section 3/////////////////////////////////////
+///////////////////////////////////////////////////
+
 const twoodButton = document.querySelector('#submit-twood');
+//identify button 
+
 twoodButton.addEventListener('click',sendTwood);
+//listens to the button, when it's clicked, runs sendTwood
+
+twoodToDelete = document.querySelector('.twood');
+twoodToDelete.remove();
 
 function sendTwood(){
-  // getText()
+  // finds the input box
   const txtInputButton = document.querySelector('#twood-input');
+  // reads the text that the user has put in the input box
   const txt = txtInputButton.value;
-  console.log(txt);
-  if(txt === ''){
-  alert('Please enter some text');
-  } else{
-    displayText = document.querySelector('.twood');
-    displayText.innerText = txt;
-  }
+  //finds the element that displays the twood
+    if(txt === ''){
+   alert('Please enter some text');
+   } else{
+const sentTwood = document.createElement("div")
+sentTwood.className = "twood";
+console.log(sentTwood);
+sentTwood.innerText = txt;
 
-
-  // overwriteExistingText
-
+const container = document.querySelector('.twoods-container')
+container.appendChild(sentTwood);
+   }
 
 }
 
+// const txt1 = 'ya'
 
-const txtInputButton = document.querySelector('#twood-input');
-const txt = txtInputButton.innerText;
+// const sentTwood = document.createElement("div")
+// sentTwood.className = "twood";
+// console.log(sentTwood);
+// sentTwood.innerText = txt1;
 
-console.log(txt);
-console.log(txt === '');
-console.log('its working');
+// const container = document.querySelector('.twoods-container')
+// container.appendChild(sentTwood);
+
